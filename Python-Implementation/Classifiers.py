714f09e5fb68f7fc2a537f67eca34371717ffb57
dflorit <danielaflorit@knights.ucf.edu>
2016-12-05 21:42:37 -0500

    Done with Issue #16
    Implemented Neural Network classifier in Classifiers.py
    It includes some setters for the parameters. Still need to include
    other setters and understand the role of those parameters.

--- Python-Implementation/Classifiers.py
+++ Python-Implementation/Classifiers.py
@@ -90,5 +90,72 @@ class SVC_Classifier(Classifier):
 		decision_function_shape = self.decision_function_shape, random_state = self.random_state)
 		
 		self.clf.set_params(**params)
+
+#Neural Network implementation using MLPClassifier()
+class NN_Classifier(Classifier):
+	def __init__(self):
+		super(NN_Classifier, self).__init__()
+		self.clf = MLPClassifier()
+		
+		# default parameters
+		self.activation='relu'
+		self.alpha=1e-05
+		self.batch_size='auto'
+		self.beta_1=0.9
+		self.beta_2=0.999
+		self.early_stopping=False
+		self.epsilon=1e-08
+		self.hidden_layer_sizes=(5, 2)
+		self.learning_rate='constant'
+		self.learning_rate_init=0.001
+		self.max_iter=200
+		self.momentum=0.9
+		self.nesterovs_momentum = True
+		self.power_t = 0.5
+		self.random_state = 1
+		self.shuffle = True
+		self.solver = 'lbfgs'
+		self.tol=0.0001
+		self.validation_fraction=0.1
+		self.verbose=False
+		self.warm_start=False
+		
+		self.params = dict( activation = self.activation, alpha = self.alpha, \
+		batch_size = self.batch_size, beta_1 = self.beta_1, beta_2 = self.beta_2, \
+		early_stopping = self.early_stopping, epsilon = self.epsilon, \
+		hidden_layer_sizes = self.hidden_layer_sizes, learning_rate = self.learning_rate, \
+		learning_rate_init = self.learning_rate_init, max_iter = self.max_iter, \
+		momentum = self.momentum, nesterovs_momentum = self.nesterovs_momentum, \
+		power_t = self.power_t, random_state = self.random_state, shuffle = self.shuffle, \
+		solver = self.solver, tol = self.tol, validation_fraction = self.validation_fraction, \
+		verbose = self.verbose, warm_start = self.warm_start)
+		
+	def train(self):
+		feature_vectors_training = self.training_data.get_feature_vectors()
+		labels_training = self.training_data.get_labels()
+		self.clf.fit(feature_vectors_training, labels_training)
+	
+	def predict(self):
+		feature_vectors_testing = self.testing_data.get_feature_vectors()
+		predictions = self.clf.predict(feature_vectors_testing)
+		self.testing_data.set_predictions(predictions)
+	
+	def get_parameters(self):
+		return self.params
+	
+	#setters for different parameters. This does not set the parameters on the classifier
+	#just the global variables for the class
+	def set_hidden_layer_sizes(self, hidden_layer_sizes):
+		self.hidden_layer_sizes = hidden_layer_sizes
+		
+	def set_tol(self, tol):
+		self.tol = tol
+		
+	def set_alpha(self, alpha):
+		self.alpha = alpha
+	
+	#this function applies all changes to the parameters in the classifier
+	def set_parameters(self):
+		self.clf.set_params(**self.params)
 		
 		
